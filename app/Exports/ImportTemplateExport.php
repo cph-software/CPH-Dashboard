ede35e763d7c43e5995d30bab4b46237d023d0cf
Muh Agil Zakaria <muhagil282004@gmail.com>
2026-04-04 12:42:04 +0800

    fix: change match to switch for PHP 7.4 compatibility in template download

--- app/Exports/ImportTemplateExport.php
+++ app/Exports/ImportTemplateExport.php
@@ -17,18 +17,27 @@ class ImportTemplateExport implements WithMultipleSheets
     {
         // Require the file containing the smaller classes since they don't match PSR-4 file naming
         require_once __DIR__ . '/ImportTemplateSheets/OtherTemplateSheets.php';
-
-        return match ($this->module) {
-            'Tyre Master'      => [new ImportTemplateSheets\TyreMasterSheet()],
-            'Vehicle Master'   => [new ImportTemplateSheets\VehicleMasterSheet()],
-            'Movement History' => [new ImportTemplateSheets\MovementHistorySheet()],
-            'Tyre Brand'       => [new ImportTemplateSheets\TyreBrandSheet()],
-            'Tyre Size'        => [new ImportTemplateSheets\TyreSizeSheet()],
-            'Tyre Pattern'     => [new ImportTemplateSheets\TyrePatternSheet()],
-            'Failure Codes'    => [new ImportTemplateSheets\FailureCodesSheet()],
-            'Locations'        => [new ImportTemplateSheets\LocationsSheet()],
-            'Segments'         => [new ImportTemplateSheets\SegmentsSheet()],
-            default            => [new ImportTemplateSheets\TyreMasterSheet()],
-        };
+        switch ($this->module) {
+            case 'Tyre Master':
+                return [new ImportTemplateSheets\TyreMasterSheet()];
+            case 'Vehicle Master':
+                return [new ImportTemplateSheets\VehicleMasterSheet()];
+            case 'Movement History':
+                return [new ImportTemplateSheets\MovementHistorySheet()];
+            case 'Tyre Brand':
+                return [new ImportTemplateSheets\TyreBrandSheet()];
+            case 'Tyre Size':
+                return [new ImportTemplateSheets\TyreSizeSheet()];
+            case 'Tyre Pattern':
+                return [new ImportTemplateSheets\TyrePatternSheet()];
+            case 'Failure Codes':
+                return [new ImportTemplateSheets\FailureCodesSheet()];
+            case 'Locations':
+                return [new ImportTemplateSheets\LocationsSheet()];
+            case 'Segments':
+                return [new ImportTemplateSheets\SegmentsSheet()];
+            default:
+                return [new ImportTemplateSheets\TyreMasterSheet()];
+        }
     }
 }
